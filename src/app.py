3ef6244161193b192ac0f3a9a9a2d95cc5b501ee
Martin-cedres <martinfernandocedres@gmail.com>
2024-11-06 02:21:13 +0000

    TRABAJO DE FAMILY STATIC API MARTIN CEDRES

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+Este módulo se encarga de iniciar el servidor API, cargar la base de datos y agregar los puntos finales.
 """
 import os
 from flask import Flask, request, jsonify, url_for
@@ -8,35 +9,79 @@ from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
 #from models import Person
 
+
+# Crea la aplicación Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+# Crea el objeto 'jackson_family' utilizando la estructura de datos definida en 'datastructures'
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+# Maneja errores globalmente y devolverlos como JSON
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
+#  Endpoint para generar el mapa del sitio con todos los endpoints disponibles
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
 
-    # this is how you can use the Family datastructure by calling its methods
+    # así es como puedes usar la estructura de datos de la Familia llamando a sus métodos
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+   
+    return jsonify(members), 200
+
+# Verifica si los campos obligatorios existen en el cuerpo de la solicitud
+@app.route('/member', methods=['POST'])
+def post_member(): 
+    body = request.get_json(silent = True)  
+    if body is None: 
+        return jsonify({'msg': 'No fue enviada informacion en el body'}), 400 
+    if 'first_name' not in body: 
+        return jsonify({'msg': 'El campo first_name es obligatorio'}), 400
+    if 'age' not in body: 
+        return jsonify({'msg': 'El campo age es obligatorio'}), 400
+    if 'lucky_numbers' not in body: 
+        return jsonify({'msg': 'El campo lucky_numbers es obligatorio'}), 400
+    
+    # Crear un nuevo miembro usando la información recibida en el cuerpo de la solicitud    
+    new_member =  {
+            'first_name': body ['first_name'], 
+            'last_name': jackson_family.last_name,
+            'age': body['age'],
+            'lucky_numbers': body['lucky_numbers']
+             }
+    if 'id' in body:
+        new_member['id']=body['id'] #Si me envian un id a traves del json que me lo agrege
+   
+   # Llamar al método 'add_member' de la estructura de datos para agregar el nuevo miembro
+    member = jackson_family.add_member(new_member)
+    return jsonify({'msg': 'Ok', 'member': member }), 200
+
+# Endpoint para obtener un miembro específico por su ID
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member=jackson_family.get_member(id) # Buscar al miembro con el ID dado
+    if not member: 
+        return jsonify({'msg': 'No existe miembro con el ID indicado'}), 400 # Si no se encuentra al miembro, devolver un error
+    return jsonify(member), 200 # Si se encuentra, devolver los datos del miembro
+
+# Endpoint para eliminar un miembro específico por su ID
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member=jackson_family.delete_member(id)
+    if not member['done']: 
+        return jsonify({'msg': 'No existe miembro con el ID indicado'}), 400
+    return jsonify(member), 200 # Si se eliminó correctamente, devolver una confirmación
 
 
-    return jsonify(response_body), 200
+    
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,36 +1,70 @@
 
 """
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
+actualice este archivo para implementar los siguientes métodos ya declarados:
+- add_member: debe agregar un miembro a la lista self._members
+- eliminar_miembro: debe eliminar un miembro de la lista self._members
+- update_member: debe actualizar un miembro de la lista self._members
+- get_member: debe devolver un miembro de la lista self._members
+
 """
 from random import randint
 
 class FamilyStructure:
     def __init__(self, last_name):
+         # El apellido de la familia se asigna al objeto
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {'id': self._generateId(), 
+             'first name': 'John', 
+             'last name': self.last_name,
+             'age': '33', 
+             'lucky numbers': [7, 13, 22],
+             },
+             {'id': self._generateId(), 
+             'first name': 'Jane', 
+             'last name': self.last_name,
+             'age': '35', 
+             'lucky numbers': [10, 14, 3]},
+             {'id': self._generateId(), 
+             'first name': 'Jimmy', 
+             'last name': self.last_name,
+             'age': '5', 
+             'lucky numbers': [1]}
+             ]
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
+    #  use este método para generar ID de miembros aleatorios al agregar miembros a la lista
     def _generateId(self):
         return randint(0, 99999999)
-
+    
+    
+    # Método para agregar un nuevo miembro a la familia
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        # Si el miembro no tiene un ID, se genera uno automáticamente
+        if not member['id']:
+            member['id']=self._generateId()
+        # Añadir el miembro a la lista de miembros
+        self._members.append(member)
+        # Devolver el miembro agregado
+        return member
+    
+ 
+     # Método para eliminar un miembro de la familia por su ID
+    def delete_member(self, id):       
+        for member in self._members:  # Itera sobre la lista de miembros
+            if member['id']==id: # Si se encuentra el miembro con el ID dado
+                self._members.remove(member) # Eliminar el miembro de la lista
+                return {'done': True} # Devolver un mensaje indicando que se eliminó correctamente
+        return {'done': False} # Si no se encontró el miembro, devolver un mensaje de error
+    
 
-    def delete_member(self, id):
-        # fill this method and update the return
-        pass
+    # Método para obtener un miembro específico por su ID
+    def get_member(self, id): 
+       for member in self._members: # Itera sobre la lista de miembros
+            if member['id']==id:  # Si se encuentra el miembro con el ID dado
+                return member # Devolver el miembro encontrado
 
-    def get_member(self, id):
-        # fill this method and update the return
-        pass
-
-    # this method is done, it returns a list with all the family members
+    # este método está hecho, devuelve una lista con todos los miembros de la familia
     def get_all_members(self):
         return self._members
